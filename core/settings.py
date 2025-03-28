64fc1479b854dbaf9be8a096907390b59feb11dc
Emin Education <143920615+bakueduaz@users.noreply.github.com>
2025-03-28 21:51:39 +0000

    changed

--- core/settings.py
+++ core/settings.py
@@ -17,9 +17,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "https://bakueduaz.github.io",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +45,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://bakueduaz.github.io",
+]
+
 
 ROOT_URLCONF = "core.urls"
 
